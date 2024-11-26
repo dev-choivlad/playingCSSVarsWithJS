bdcf8197ec55a82ce6a7e13d8fb96aa5263bd57e
Choi Vlad <dev.choivlad@gmail.com>
2024-11-25 19:31:58 -0800

    getElem and handleUpdate funcs

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 import * as flsFunctions from "./modules/check-webp.js";
+import {getElems} from "./modules/project.js";
 
+getElems();
 
 flsFunctions.isWebp();
--- src/js/modules/project.js
+++ src/js/modules/project.js
@@ -1,1 +1,13 @@
-//export function handle
+export function getElems() {
+	const inputs = document.querySelectorAll(".controls input");
+
+	inputs.forEach(input => {
+		input.addEventListener("change", handleUpdate);
+		input.addEventListener("mousemove", handleUpdate);
+	})
+}
+
+export function handleUpdate() {
+	const suffix = this.dataset.sizing || "";
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
